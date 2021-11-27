ba74e456edf98262fbf777dfe2ff4bab345ccd23
andrewpetrav <61365800+andrewpetrav@users.noreply.github.com>
2021-11-26 19:47:30 -0600

    Update LinkedList.java

--- Project/LinkedList.java
+++ Project/LinkedList.java
@@ -1,16 +1,47 @@
-//Linked List is bounded
-public class LinkedList<T> { //superclass of unsorted linked list and sorted linked list
-	NodeLinked<T> head; //first node in list
-	NodeLinked<T> tail; //last node in list
-	NodeLinked<T> current; //current node being pointed to
-	int length; //holds length of list
+
+public class LinkedList<T extends Comparable<T>> implements CollectionInterface<T> {
+	public LLNode<T> head; //holds the head node
+	public LLNode<T> curr;
+	int length; //how many nodes the list has
+	
+	private class LLNode<T>{
+		T data;
+		LLNode<T> next;
+		
+		private LLNode(T d, LLNode<T> n) { //non default constructor
+			data=d;
+			next=n;
+		}
+	}
 	
 	public LinkedList() { //default constructor
 		head=null;
-		tail=null;
-		current=null;
 		length=0;
 	}
+	
+	public void add(T element) {
+		head=new LLNode<T>(element, head);
+		length++;
+	}
+	
+	public void print() {
+		curr=head;
+		while(curr!=null) { //while not null
+			System.out.println(curr.data);
+			curr=curr.next;
+		}
+	}
+	
+	public boolean seqSearch(T element) {
+		curr=head;
+		while(curr!=null) {
+			if(curr.data.equals(element)) {
+				return true; //if found
+			}
+		}
+		return false; //not found
+	}
+	
 	
 
 }
