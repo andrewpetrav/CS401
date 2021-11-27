ffaad5a87ac3b22743359483da4ad395354dd3a5
andrewpetrav <61365800+andrewpetrav@users.noreply.github.com>
2021-11-26 19:48:26 -0600

    Create CollectionInterface.java

---
+++ Project/CollectionInterface.java
@@ -0,0 +1,13 @@
+
+public interface CollectionInterface<T> {
+
+	public void add(T element);
+	public void print();
+	//public boolean search(T target);
+
+	//public void sort();
+	//sequential search
+	//insertion sort
+	
+	
+}
